4d7825c6a1edc4d2550e7fd7e44a2c2bc27e2730
Dewayne Allen <dewayneallen76@gmail.com>
2016-07-27 14:48:29 -0500

    adlister group exercise. welcome page.

---
+++ public/adlister.welcome.php
@@ -0,0 +1,166 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>eProps</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">eProps</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Register</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="#">Create Ad</a>
+                    </li>
+                    <li>
+                        <a href="#">Listings</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer" style="background-image:url(img/hollywood.jpg" >
+            <h1>Welcome to eProps, the online auction site for used movie props!</h1>
+            <p>Have you ever watched a movie and thought, <em>"I need one of those!"</em>.  eProps is the place where you can buy, sell, or trade collectible used movie props with other fans or collectors. Our easy to use website will guide you through the process from beginning to end. Click below to get started!</p>
+            <p><a class="btn btn-primary btn-large">Search available items</a>
+            </p>
+        </header>
+
+        <hr>
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Newest Items</h3>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Page Features -->
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="img/thorhammer.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Mjolnir</h3>
+                        <p>"Whosoever holds this hammer, if he be worthy, shall possess the power of Thor." This was used in the filming of both Avengers movies.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="img/easyrider.png" alt="">
+                    <div class="caption">
+                        <h3>Captain America</h3>
+                        <p>Own the helmet worn by Peter Fonda in the classic movie Easy Rider.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="img/jacket.jpeg" alt="">
+                    <div class="caption">
+                        <h3>TBirds Jacket</h3>
+                        <p>Impress your friends wearing the jacket that John Travolta wore in Grease.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="img/jaws.jpg" alt="">
+                    <div class="caption">
+                        <h3>Jaws Props</h3>
+                        <p>You're going to need a bigger boat. Own the yellow barrels used to try to catch the "beast" in Jaws.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; JJBD, INC. 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
