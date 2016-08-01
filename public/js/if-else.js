69cd749055fd37af345d208ba1dce023b332ddd2
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-01 14:06:50 -0500

    CREATE FUNCTION TO REFACTOR EXERCISE FROM IF/ELSE STATEMENTS

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -24,40 +24,60 @@ var flipACoin = Math.floor(Math.random()* 2)
 Add an if statement to log a "Buy a car" to the console if the result is 0 and "Buy a house" if the result is 1. 
 Could this program be written using a ternary operator?*/
 
+
+// when refactoring existing code into a function
+// input - figure out what inputs change and make generally named parameters
+// process - we already have the process (in this case)
+// output - we already have the output (in this case), but not in a general form 
+// name the function after what it does or what it returns
 var person = "Cameron";
 var salesAmount = 180;
 var discount = 0.35;
 var finalAmount;
 
-if(salesAmount > 200) {
-	finalAmount = salesAmount - (salesAmount * discount);
-	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
-} else {
-	finalAmount = salesAmount;
-	console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
+// original exercise refactored to use function ...commented out if else statements 
+// are original exercise. 
+
+function displaySaleInfo(person, salesAmount, discount) {
+	
+    if(salesAmount > 200) {
+		finalAmount = salesAmount - (salesAmount * discount);
+	 	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
+    } else {
+		finalAmount = salesAmount;
+		console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
+  	}
+  	return ;
+
 }
+// call the function
+displaySaleInfo(person, salesAmount, discount);
 
 person = "Ryan"
 salesAmount = 250
+// call the function for Ryan
+displaySaleInfo(person, salesAmount, discount);
 
-if(salesAmount > 200) {
-	finalAmount = salesAmount - (salesAmount * discount);
-	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
-} else {
-	finalAmount = salesAmount;
-	console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
-}
+// if(salesAmount > 200) {
+// 	finalAmount = salesAmount - (salesAmount * discount);
+// 	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
+// } else {
+// 	finalAmount = salesAmount;
+// 	console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
+// }
 
 person = "George"
 salesAmount = 320
+// call the function for George 
+displaySaleInfo(person, salesAmount, discount);
 
-if(salesAmount > 200) {
-	finalAmount = salesAmount - (salesAmount * discount);
-	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
-} else {
-	finalAmount = salesAmount;
-	console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
-}
+// if(salesAmount > 200) {
+// 	finalAmount = salesAmount - (salesAmount * discount);
+// 	console.log(person + " purchased $" + salesAmount + " and a discount was was applied. Final amount is: $" + finalAmount.toFixed(2));
+// } else {
+// 	finalAmount = salesAmount;
+// 	console.log(person + " purchased $" + salesAmount + " and a discount was not applied. Final amount is: $" + finalAmount.toFixed(2));
+// }
 /*HEB has an offer for the clients that buy products amounting more than $200. The discount is 35% off the purchase. 
 Write a JS program, using conditionals, that logs to the browser, how much Ryan, Cameron and George need to pay. 
 We know that Cameron bought $180, Ryan $250 and George $320. Your program will have to display a line with the name
@@ -65,3 +85,4 @@ We know that Cameron bought $180, Ryan $250 and George $320. Your program will h
 The output of your code should be similar to the following one:
 Luis bought $100.00, no discount was applied. Final payment: $100.00.
 Zach bought $220.00, discount was applied. Final payment: $143.00.*/
+
