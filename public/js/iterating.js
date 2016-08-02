1c8fe91278f4ea2a5c95de147d1f33e242b85fc0
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-02 14:21:22 -0500

    ITERATING JS FILE CLASSROOM WORK

---
+++ public/js/iterating.js
@@ -0,0 +1,17 @@
+(function(){
+    "use strict";
+
+var names = ["Hector", "Joey", "Ben", "Dan"];
+console.log(names);
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+console.log(names.length);
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+console.log(names [0]);
+console.log(names [1]);
+console.log(names [2]);
+console.log(names [3]);
+    // TODO: Create log statements that will print each of the names array elements individually.
+})();
