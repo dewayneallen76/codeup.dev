6b624032ef4c803644f895d074d37ff08d438cfc
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-03 17:08:05 -0500

    JSON CLASSROOM LECTURE NOTES

---
+++ public/js/json-notes.js
@@ -0,0 +1,29 @@
+"use strict";
+//Valid JSON example. Data-interchange format that is easy for humans to ready and write.
+//widely used in cloud based services and web application programming interfaces (APIs)
+var car = {
+	"make": ,
+	"model": {
+		"name": "sonata",
+		"suspension": "sport",
+		"trim": null;
+	},
+	"year": 1998;
+	"miles": 200000,
+	"mpg": 19,
+	"color": "blue",
+	"curentOwner": {
+		"firstName": "Ryan",
+		"lastName": "Orsinger",
+	},
+	"previousOwners": [
+		{
+			"firstName": "Bob",
+			"lastName": "Bobberson"
+		},
+		{
+			"firstName": "Dave",
+			"lastName": "Davies"
+		}
+	]
+};
