2e2cc30d34ab8e9da7a0ffbe9fc4d2567a0cca49
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-09 10:41:52 -0500

    UPDATES TO CALCULATOR FILES, DISPLAYS FUNCTIONING PROPERLY NEED TO WORK ON MATH

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -2,26 +2,43 @@
 "use strict";
 console.log('javascript linked');
 
-var equals = document.getElementsByClassName('equals');
+var equals = document.getElementById('btnEquals');
 var clear = document.getElementById('btnClear');
-var buttons = document.getElementsByClassName('buttons');
+var numbers = document.getElementsByClassName('numbers');
 var leftInput = document.getElementById('leftInput');
-var operator = document.getElementById('operator');
+var operator = document.getElementById('operatorInput');
 var rightInput = document.getElementById('rightInput');
 var operators = document.getElementsByClassName('operators');
+var answer = 0
+// for (var i = 0; i < operators.length; i++) {
+// 	operators[i].addEventListener('click', handleButton)
+// }
 
-for (var i = 0; i < operators.length; i++) {
-	operators[i].addEventListener('click', handleButton)
+for (var i = 0; i < numbers.length; i++) {
+	numbers[i].addEventListener('click',handleButton)
 }
-
-for (var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener('click',handleButton)
-
+for (var i = 0; i < operators.length; i++) {
+	operators[i].addEventListener('click',handleButton)
 }
 
 // Now I need to get the handleButton function to display in the inputs on top of the calculator
 function handleButton(){
-	leftInput.value += this.innerHTML;
+	console.log(this.className);
+	if (this.className.includes("operators")) {
+		operator.value = this.innerHTML;
+	} else if (operator.value != ''){
+		rightInput.value += this.innerHTML;
+	} else if (this.className.includes("numbers")) {
+		leftInput.value += this.innerHTML;
+	}
+////FERNANDO HELPED ME WITH THE CODE BELOW//////////////
+	// if (operator.value != '' && this.className.includes("keys")){
+	// 	rightInput.value += this.innerHTML;
+	// } else if (operator.value == '' && this.className.includes("keys")) {
+	// 	leftInput.value += this.innerHTML;
+	// } else if (this.className.includes("operators") && operator.value == '') {
+	// 	operator.value = this.innerHTML;
+	// } 
 	// operator.value = operator.innerHTML;
 	// rightInput.value += rightInput.innerHTML;
 	// console.log(leftInput.value);
@@ -32,6 +49,11 @@ clear.addEventListener('click',function(){
 	leftInput.value = '';
 	rightInput.value = '';
 	operator.value = '';
+})
+// Equals
+equals.addEventListener('click',function(){
+
+	console.log(leftInput.value);
 })
 
 
