2e8b37ab5ae2f9c50d05cf00e04c78933bea3cc5
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-12 15:05:56 -0500

    JQUERY FAQ, TRAVERSING EXERCISES

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -1,11 +1,23 @@
 (function(){
+
 "use strict";
+
 console.log('js linked');
 $(document).ready(function() {
     $('#show-information').click(function(event) {
         event.preventDefault();
         $('dd').toggleClass('invisible');
     });
+
+$('ul').each(function(element, index) {
+	$(this).children('li').first().css('font-weight', 'bold');
+	});
+
+$('li').click(function(event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('invisible');
+    });
+ 
 });
 
-})();
+})(); 
