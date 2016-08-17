5537aed09f6e8955a92b111ad1bc1b549d0a8525
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-16 22:47:30 -0500

    ADDED BACKGROUND COLOR, ADDED LAT, LONG AND SUBMIT BUTTON, ADDED COMMENTS TO CSS AND JS

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -1,4 +1,4 @@
-// (function(){
+(function(){
 "use strict";
 console.log('JS linked');
 
@@ -18,37 +18,40 @@ $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
 }).fail(function (xhr, err, msg) {
 	alert('Failed to connect!');
 });
-// console.log(weatherObject);
+// Function for current weather 
 function addWeather (weatherObject) {
 	var content = ''; 
 		content += '<h3>' + parseInt(weatherObject.list[0].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[0].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[0].weather[0].icon +'.png">';
+		content += '<h4>' + 'Wind: ' + weatherObject.list[0].speed + '</h4>';
 		content += '<h4>' + 'Humidity: ' + weatherObject.list[0].humidity + '</h4>';
 		content += '<h4>' + 'Pressure: ' + weatherObject.list[0].pressure + '</h4>';
 		content += weatherObject.list[0].weather[0].description;
 	$('#todayWeather').append(content);
 };
-
+// Function for second day weather 
 function tomorrowWeather (weatherObject) {
 	var content = '';
 		content += '<h3>' + parseInt(weatherObject.list[1].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[1].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[1].weather[0].icon +'.png">';
+		content += '<h4>' + 'Wind: ' + weatherObject.list[1].speed + '</h4>';
 		content += '<h4>' + 'Humidity: ' + weatherObject.list[1].humidity + '</h4>';
 		content += '<h4>' + 'Pressure: ' + weatherObject.list[1].pressure + '</h4>';
 		content += weatherObject.list[1].weather[0].description;
 	$('#tomorrowWeather').append(content);
 };
-
+// Function for third day weather
 function thirdDayWeather (weatherObject) {
 	var content = '';
 		content += '<h3>' + parseInt(weatherObject.list[2].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[2].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[2].weather[0].icon +'.png">';
+		content += '<h4>' + 'Wind: ' + weatherObject.list[2].speed + '</h4>';
 		content += '<h4>' + 'Humidity: ' + weatherObject.list[2].humidity + '</h4>';
 		content += '<h4>' + 'Pressure: ' + weatherObject.list[2].pressure + '</h4>';
 		content += weatherObject.list[2].weather[0].description;
 	$('#thirdDayWeather').append(content);
 };
-
+// Adding google map
 var mapOptions = {
           zoom: 4,
           center: {
@@ -56,7 +59,7 @@ var mapOptions = {
               lng:  -95.7129
           }
         };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
 
-// })();	
+})();	
