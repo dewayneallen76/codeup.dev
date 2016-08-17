d52a8af1f85978642a46b436a094693b8831de3e
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-17 11:21:38 -0500

    ADDED INPUT LINES FOR LATITUDE AND LONGITUDE. USER CAN ENTER LATITUDE AND LONGITUDE FOR A RETURN WEATHER FOR THAT LOCATION BY CLICKING SUBMIT. WEATHER WILL BE DISPLAYED BELOW SAN ANTONIO WEATHER.

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -1,26 +1,19 @@
-(function(){
+// (function(){
 "use strict";
 console.log('JS linked');
 
 const myAPIKey = '9045f328001b8cd61b813c19463bd371'; // created constant for API Key 
 var weatherObject; 
-
-$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+var weatherOptions = {
     APPID: myAPIKey,
     q:     "San Antonio, TX",
     units: "imperial"
-}).done(function (data) {
-	weatherObject = data;
-	console.log(weatherObject);
-	addWeather(weatherObject);
-	tomorrowWeather(weatherObject);
-	thirdDayWeather(weatherObject);
-}).fail(function (xhr, err, msg) {
-	alert('Failed to connect!');
-});
+};
+
 // Function for current weather 
 function addWeather (weatherObject) {
 	var content = ''; 
+		content = '<h4>' + weatherObject.city.name + '</h4>';
 		content += '<h3>' + parseInt(weatherObject.list[0].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[0].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[0].weather[0].icon +'.png">';
 		content += '<h4>' + 'Wind: ' + weatherObject.list[0].speed + '</h4>';
@@ -32,6 +25,7 @@ function addWeather (weatherObject) {
 // Function for second day weather 
 function tomorrowWeather (weatherObject) {
 	var content = '';
+		content += '<h4>' + weatherObject.city.name + '</h4>';
 		content += '<h3>' + parseInt(weatherObject.list[1].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[1].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[1].weather[0].icon +'.png">';
 		content += '<h4>' + 'Wind: ' + weatherObject.list[1].speed + '</h4>';
@@ -43,6 +37,7 @@ function tomorrowWeather (weatherObject) {
 // Function for third day weather
 function thirdDayWeather (weatherObject) {
 	var content = '';
+		content = '<h4>' + weatherObject.city.name + '</h4>';
 		content += '<h3>' + parseInt(weatherObject.list[2].temp.day) + '&deg' + '/' + parseInt(weatherObject.list[2].temp.min) + '&deg' + '</h3>';
 		content += '<img src = "http://openweathermap.org/img/w/'+ weatherObject.list[2].weather[0].icon +'.png">';
 		content += '<h4>' + 'Wind: ' + weatherObject.list[2].speed + '</h4>';
@@ -62,4 +57,35 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
 
-})();	
+// inputting latitude and longitude 
+function getWeatherData () {
+	$.get("http://api.openweathermap.org/data/2.5/forecast/daily",weatherOptions
+		).done(function (data) {
+			weatherObject = data;
+			console.log(weatherObject);
+			addWeather(weatherObject);
+			tomorrowWeather(weatherObject);
+			thirdDayWeather(weatherObject);
+		}).fail(function (xhr, err, msg) {
+			alert('Failed to connect!');
+	});
+}
+getWeatherData();
+// initialize getWeatherData
+
+$('#submit').click (function() {
+var inputLatitude = $('#inputLatitude').val();
+var inputLongitude = $('#inputLongitude').val();
+	weatherOptions.lat = inputLatitude;
+	weatherOptions.lon = inputLongitude;
+	weatherOptions.q = '';
+	getWeatherData();
+
+alert('submit working'); //alert to verify that the button is working 
+console.log(inputLongitude); //logging inputted longitude
+console.log(inputLatitude); //logging inputted latitude
+});
+
+
+
+// })();	
