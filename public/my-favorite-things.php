bec87f5568eb1002b7ca3ef3d9b13fbcbf829294
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-07 15:18:17 -0500

    Create an array of your favorite things. This array should contain at least five things. We will need to loop through this array and display each element inside your HTML.

---
+++ public/my-favorite-things.php
@@ -0,0 +1,28 @@
+<?php 
+
+$favoriteThings = ['God', 'Guns', 'Family', 'Murica', 'Apple Pie'];
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Favorite Things</title>
+</head>
+<style>
+	h1, ul {
+		text-align: center;
+		color: green;
+		font-family: "Lucida Console", Monaco, monospace;
+	}
+</style>
+<body>
+	<table style = "width:100%">
+	<h1>My Favorite Things</h1>
+    <?php foreach ($favoriteThings as $things) { ?>
+        <th><?php echo $things; ?></th>
+    <?php } ?>
+    </table>
+</body>
+</html>
