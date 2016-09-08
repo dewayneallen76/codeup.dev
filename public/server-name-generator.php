82996fa1ccbb9367f2f8d0db2bcac444ec42d1de
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-08 10:32:42 -0500

    REFACTRORED USING PAGE CONTROLLER FUNCTION

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,13 +1,24 @@
-<?php 
-// arrays for adjectives and nouns
-$adjective = ['Amazing', 'Bumpy', 'Courageous', 'Droopy', 'Edible', 'Fabulous', 'Gargantuan', 'Hairy', 'Interesting', 'Jumpy'];
-$noun = ['Tree', 'Airplane', 'House', 'Dog', 'Lizard', 'Camel', 'Rocket', 'Snake', 'Mouse', 'Baby'];
+<?php
 // Function to create random name from $adjective array and from $noun array
 function randomServerName($array) 
 { 	
 $random = mt_rand(0,9);
 return $array[$random];
 }
+// Added function to create random server name from both arrays 
+function generator($element1, $element2) 
+{
+return randomServerName($element1) . " " . randomServerName($element2);
+}
+// refactored to add page controller, moved arrays to pageController function 
+function pageController() {
+$adjective = ['Amazing', 'Bumpy', 'Courageous', 'Droopy', 'Edible', 'Fabulous', 'Gargantuan', 'Hairy', 'Interesting', 'Jumpy'];
+$noun = ['Tree', 'Airplane', 'House', 'Dog', 'Lizard', 'Camel', 'Rocket', 'Snake', 'Mouse', 'Baby'];	
+	$data = [];
+	$data['randomServerName'] = generator($adjective, $noun);
+return $data;
+}
+extract(pageController());
 
 ?>
 <!-- HTML for output -->
@@ -27,7 +38,7 @@ return $array[$random];
 </style>
 <body>
 	<h1>Server Name:</h1>
-	<h1><?= randomServerName($adjective) . " " . randomServerName($noun); ?></h1>
+	<h1><?= $randomServerName; ?></h1>
 </body>
 </html>
 
