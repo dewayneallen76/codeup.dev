eb9794a21e0902bd671ea08734e8ccaf93addf49
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-08 13:56:23 -0500

    COUNTER CLASSROOM EXERCISE: Create a file called counter.php using the page controller template. In your HTML, you will need two links. One that says up and another that says down. Add a heading that shows a number representing the current counter value.

---
+++ public/counter.php
@@ -0,0 +1,25 @@
+<?php 
+function pageController() 
+{
+	$count = 0;
+
+
+}
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Counter</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    	<h1>Counter: 0</h1>
+    	<a class="btn btn-default" href="codeup.dev/counter.php" role="button">Up</a>
+    	<a class="btn btn-default" href="codeup.dev/counter.php" role="button">Down</a>
+    </div>
+</body>    
