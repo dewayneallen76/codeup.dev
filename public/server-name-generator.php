df16be5c69847e374ab8a149d2fc7a7fd93d2e9b
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-09 12:00:18 -0500

    ADDED TABS TO MAKE CODE CLEANER

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,24 +1,24 @@
 <?php
 // Function to create random name from $adjective array and from $noun array
-function randomServerName($array) 
-{ 	
-$random = mt_rand(0,9);
-return $array[$random];
-}
+	function randomServerName($array) 
+	{ 	
+		$random = mt_rand(0,9);
+		return $array[$random];
+	}
 // Added function to create random server name from both arrays 
-function generator($element1, $element2) 
-{
-return randomServerName($element1) . " " . randomServerName($element2);
-}
+	function generator($element1, $element2) 
+	{
+		return randomServerName($element1) . " " . randomServerName($element2);
+	}
 // refactored to add page controller, moved arrays to pageController function 
-function pageController() {
-$adjective = ['Amazing', 'Bumpy', 'Courageous', 'Droopy', 'Edible', 'Fabulous', 'Gargantuan', 'Hairy', 'Interesting', 'Jumpy'];
-$noun = ['Tree', 'Airplane', 'House', 'Dog', 'Lizard', 'Camel', 'Rocket', 'Snake', 'Mouse', 'Baby'];	
-	$data = [];
-	$data['randomServerName'] = generator($adjective, $noun);
-return $data;
-}
-extract(pageController());
+	function pageController() {
+		$adjective = ['Amazing', 'Bumpy', 'Courageous', 'Droopy', 'Edible', 'Fabulous', 'Gargantuan', 'Hairy', 'Interesting', 'Jumpy'];
+		$noun = ['Tree', 'Airplane', 'House', 'Dog', 'Lizard', 'Camel', 'Rocket', 'Snake', 'Mouse', 'Baby'];	
+		$data = [];
+		$data['randomServerName'] = generator($adjective, $noun);
+		return $data;
+	}
+	extract(pageController());
 
 ?>
 <!-- HTML for output -->
