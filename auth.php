34a127d3472bd41a5bce9461de10f114594a0c4e
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-15 10:52:18 -0500

    COMLPETED CLASS FUNCTIONS FOR CLASSROOM EXERCISE

--- auth.php
+++ auth.php
@@ -5,43 +5,41 @@ class Auth
 {
 
 	public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
-
+// will take in a $username and $password. If the $username is guest and the $password matches the hashed password above then set the LOGGED_IN_USER session variable as before. Use the Log class to log an info message: "User $username logged in.". If either the username or password are incorrect then log an error: "User $username failed to log in!". You will need to use the PHP method password_verify() to check the password hash.
 	public static function attempt($username, $password) 
 	{
+			$newFile = new Log();
 			if($username == 'guest' && password_verify($password, Auth::$password) == 'password') {
-				header("Location: /authorized.php");
 				$_SESSION['logged in user'] = $username;
-				$newFile = new Log();
 				$newFile->filename = "log-" . date('Y-m-d') . '.log';
-				$newFile->info("User {$username} successfully logged in with {$password}");
+				$newFile->info("{$username} successfully logged in.");
+				header("Location: /authorized.php");
 				die;
 			} else {
-				$newFile = new Log();
 				$newFile->filename = "log-" . date('Y-m-d') . '.log';
-				$message = "Login Failed. Try Again";
+				$newFile->info("{$username} failed to login.");
+				return false;
 			}
 	}
-
+// will return a boolean whether or not a user is logged in.
 	public static function check() 
 	{
-		// if(!empty($_SESSION)) {
 			if($_SESSION['logged in user'] != 'guest') {
 				header("Location: /login.php");
-				return true;
 				die;
+				return true;
 			}else {
 				return false; 
-			}
-		// }		
+			}		
 	}
-
+// will return the username of the currently logged in user.
 	public static function user() 
 	{
 		if(isset($_SESSION['logged in user'])) {
 			return $_SESSION['logged in user'];
 		} 
 	}
-
+// will end the session
 	public static function logout() 
 	{
     	session_unset();
