7ce7158e7d8931ae36d286eefe6a961a53ae155a
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-29 11:42:11 -0500

    Update your Input class with three new methods:
    public static function getString($key)
    public static function getNumber($key)
    
    Each of these methods should use the get() method internally to retrieve the value from $_POST or $_GET. If the values does not exist, or match the expected type, throw an exception.

--- input.php
+++ input.php
@@ -33,6 +33,34 @@ class Input
 		}
     }
 
+    // Update your Input class with two new methods: (PHP IV : Exceptions (Throwing))
+    // public static function getString($key)
+    public static function getString($key) 
+    {
+        if(! self::has($key))  
+        {
+            throw new Exception("Request does not have a key: '$key'"); 
+        }
+        $value = self::get($key);
+        if (gettype($value) != 'string') {
+            throw new Exception("Value at index '$key' is not a string!")
+        }
+        return $value;
+    }
+    // public static function getNumber($key)
+    public static function getNumber($key) 
+    {
+        if(! self::has($key))  
+        {
+            throw new Exception("Request does not have a key: '$key'"); 
+        }
+        $value = self::get($key);
+        if (! is_numeric($value) {
+            throw new Exception("Value at index '$key' is not a number!")
+        }
+        return $value;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
