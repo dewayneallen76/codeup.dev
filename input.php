fb340aac11a1bf9e8c8ccf465a3b92428c596b4b
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-30 14:31:11 -0500

    moved errors array and made public static in input.php

--- input.php
+++ input.php
@@ -2,6 +2,7 @@
 
 class Input
 {
+	public static $errors = [];
 	/**
 	 * Check if a given value was passed in the request
 	 *
@@ -35,29 +36,42 @@ class Input
 
 	// Update your Input class with two new methods: (PHP IV : Exceptions (Throwing))
 	// public static function getString($key)
-	
-	public static function getString($key, $default = null, $min, $max)
+	public static function getString($key, $min=0, $max=10000)
 	{
-		if (!self::has($key, $min, $max)) {
-			throw new OutOfRangeException("$key missing");
-		} else if (is_string(self::get($key, $min, $max)) && !is_numeric(self::get($key, $min, $max))) {
-			return self::get($key, $min, $max);
-		} else {
-			throw new InvalidArgumentException();
+		if(!self::has($key)) {
+			throw new OutOfRangeException("Request does not contain key: '{$key}'");
 		}
+		$value = self::get($key);
+		
+		if (gettype($value) != 'string') {
+			throw new DomainException("Value {$value} is not a string");
+		}
+		if (strlen($value) < $min || strlen($value) > $max) {
+			throw new LengthException("Length must be between {$min} and {$max}");
+		}
+		if (!is_int($min) && !is_int($max)) {
+			throw new InvalidArgumentException("Value at index '{$key}' is the wrong type");
+		}
+		
+		return $value;
 	}
 
 	// public static function getNumber($key)
 
-	public static function getNumber($key, $min, $max)
+	public static function getNumber($key, $min=0, $max=100000)
 	{
-		if (!self::has($key, $min, $max)) {
-			throw new OutOfRangeException("$key missing");
-		} else if (is_numeric(self::get($key, $min, $max))) {
-			return floatval(self::get($key, $min, $max));
-		} else {
-			throw new InvalidArgumentException();
+			
+		if (!self::has($key)) {
+			throw new OutOfRangeException("Request does not contain key: '{$key}'");
 		}
+		$value = self::get($key);
+		if ($value < $min || $value > $max) {
+			throw new RangeException("Number must be between {$min} and {$max}");
+		}
+		if (!is_int($value) && !is_int($min) && !is_int($max)){
+			throw new InvalidArgumentException("Value '{$value}' is not a number!");
+		}
+		return (int)$value;
 	}
 
 	///////////////////////////////////////////////////////////////////////////
