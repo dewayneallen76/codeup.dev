27291ce96949298642c8702c447e0a62ab0e8c03
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-30 12:26:58 -0500

    refactoring for domain exception

--- input.php
+++ input.php
@@ -2,75 +2,69 @@
 
 class Input
 {
-    /**
-     * Check if a given value was passed in the request
-     *
-     * @param string $key index to look for in request
-     * @return boolean whether value exists in $_POST or $_GET
-     */
-    public static function has($key)
-    {
-       	if(isset($_REQUEST[$key])) {
+	/**
+	 * Check if a given value was passed in the request
+	 *
+	 * @param string $key index to look for in request
+	 * @return boolean whether value exists in $_POST or $_GET
+	 */
+	public static function has($key)
+	{
+		if(isset($_REQUEST[$key])) {
 		return true;
 		} else {
 		return false; 
 		}
-    }
+	}
 
-    /**
-     * Get a requested value from either $_POST or $_GET
-     *
-     * @param string $key index to look for in index
-     * @param mixed $default default value to return if key not found
-     * @return mixed value passed in request
-     */
-    public static function get($key, $default = null)
-    {
-        if(isset($_REQUEST[$key]) == true){
+	/**
+	 * Get a requested value from either $_POST or $_GET
+	 *
+	 * @param string $key index to look for in index
+	 * @param mixed $default default value to return if key not found
+	 * @return mixed value passed in request
+	 */
+	public static function get($key, $default = null)
+	{
+		if(isset($_REQUEST[$key]) == true){
 		return $_REQUEST[$key];
 		} else {
 		return null;
 		}
-    }
+	}
 
-    // Update your Input class with two new methods: (PHP IV : Exceptions (Throwing))
-    // public static function getString($key)
-    public static function getString($key, $min, $max) 
-    {
-        if(!self::has($key)) 
-            throw new OutOfRangeException("Request does not have a key: $key"); 
-        }
-        $value = self::get($key);
+	// Update your Input class with two new methods: (PHP IV : Exceptions (Throwing))
+	// public static function getString($key)
+	
+	public static function getString($key, $default = null, $min, $max)
+	{
+		if (!self::has($key, $min, $max)) {
+			throw new OutOfRangeException("$key missing");
+		} else if (is_string(self::get($key, $min, $max)) && !is_numeric(self::get($key, $min, $max))) {
+			return self::get($key, $min, $max);
+		} else {
+			throw new InvalidArgumentException();
+		}
+	}
 
-        if (gettype($value) != 'string') || (gettype($min) != 'string') && (gettype($max) != 'string')
-        {
-            throw new InvalidArgumentException("Value at index $key is not a string!");
-        }
+	// public static function getNumber($key)
 
-        return $value;
-    }
-    // public static function getNumber($key)
-    public static function getNumber($key, $min, $max) 
-    {
-        if(!self::has($key)) 
-        {
-            throw new OutOfRangeException("Request does not have a key: $key"); 
-        }
-        $value = self::get($key);
-        
-        if (!is_numeric($value)) || (! is_numeric($min)) && (! is_numeric($max))
-        {
-            throw new InvalidArgumentException("Value at index $key is not a number!");
-        }
-        
-        return $value;
-    }
+	public static function getNumber($key, $min, $max)
+	{
+		if (!self::has($key, $min, $max)) {
+			throw new OutOfRangeException("$key missing");
+		} else if (is_numeric(self::get($key, $min, $max))) {
+			return floatval(self::get($key, $min, $max));
+		} else {
+			throw new InvalidArgumentException();
+		}
+	}
 
-    ///////////////////////////////////////////////////////////////////////////
-    //                      DO NOT EDIT ANYTHING BELOW!!                     //
-    // The Input class should not ever be instantiated, so we prevent the    //
-    // constructor method from being called. We will be covering private     //
-    // later in the curriculum.                                              //
-    ///////////////////////////////////////////////////////////////////////////
-    private function __construct() {}
+	///////////////////////////////////////////////////////////////////////////
+	//                      DO NOT EDIT ANYTHING BELOW!!                     //
+	// The Input class should not ever be instantiated, so we prevent the    //
+	// constructor method from being called. We will be covering private     //
+	// later in the curriculum.                                              //
+	///////////////////////////////////////////////////////////////////////////
+	private function __construct() {}
 }
