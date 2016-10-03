751047103df8e4ec981f84754054d0c028158e8a
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-03 15:25:12 -0500

    working on model for national parks website

---
+++ Park.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Park
+{
+	protected static $dbc;
+	protected static function dbConnect()
+    {
+        if (!self::$dbc) {
+            // @TODO: Connect to database
+            self::$dbc = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
+
+            // Tell PDO to throw exceptions on error
+            self::$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+}
+
+
+
+ ?>
