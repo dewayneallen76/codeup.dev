3111a40b1699ec1a6a8719c81bb0fc397cb4032c
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-03 10:22:15 -0500

    working on User model, downloaded file.

--- User.php
+++ User.php
@@ -1,15 +1,64 @@
-<?php 
+<?php
+
+// __DIR__ is a *magic constant* with the directory path containing this file.
+// This allows us to correctly require_once Model.php, no matter where this file is being required from.
 require_once __DIR__ . '/Model.php';
-// Create a new file name User.php. (PHP IV : Late Satic Binding)
-// In this new file, create a User class that extends Model. (PHP IV : Late Static Binding)
-// This class will be very simple, and will only contain the overridden $table property, set to users. (PHP IV Late Static Binding)
-class User extends Model 
+
+class User extends Model
 {
-	protected static $table = 'users';
+    /** Insert a new entry into the database */
+    protected function insert()
+    {
+
+        // @TODO: Use prepared statements to ensure data security
+
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+
+        // @TODO: After the insert, add the id back to the attributes array
+        //        so the object properly represents a DB record
+    }
+
+    /** Update existing entry in the database */
+    protected function update()
+    {
+        // @TODO: Use prepared statements to ensure data security
+
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+    }
+
+    /**
+     * Find a single record in the DB based on its id
+     *
+     * @param int $id id of the user entry in the database
+     *
+     * @return User An instance of the User class with attributes array set to values from the database
+     */
+    public static function find($id)
+    {
+        // Get connection to the database
+        self::dbConnect();
+
+        // @TODO: Create select statement using prepared statements
+
+        // @TODO: Store the result in a variable named $result
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+        $instance = null;
+        if ($result) {
+            $instance = new static($result);
+        }
+        return $instance;
+    }
+
+    /**
+     * Find all records in a table
+     *
+     * @return User[] Array of instances of the User class with attributes set to values from database
+     */
+    public static function all()
+    {
+        self::dbConnect();
+
+        // @TODO: Learning from the find method, return all the matching records
+    }
 }
-// Test your User class by calling User::getTableName(). Did it return the string users? YES
-// In your parent Model class, did you use self:: or static::? static::
-// Does switching them change the result? Yes, self returns the string set in the Model class
-var_dump(User::getTableName()); 
- 
- ?>
