50db1fa31d6d645086079f79c9c965b57c97578e
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-10 09:45:01 -0500

    added try catch statement to try the db connection and show error if not connected

--- db-connection.php
+++ db-connection.php
@@ -6,11 +6,14 @@ $connection = new PDO(
 	'vagrant'
 );
 
+try {
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // connection successful message
 	echo $connection->getAttribute(PDO::ATTR_CONNECTION_STATUS), PHP_EOL;
-
+} catch (PDOException $e) {
+	echo $e->getMessage(), PHP_EOL;
+}
 
 
  ?>
