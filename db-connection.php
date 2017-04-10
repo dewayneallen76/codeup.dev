749e3dfd0faff51064a41c65fff242e2d1957683
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-10 09:36:55 -0500

    updating

---
+++ db-connection.php
@@ -0,0 +1,18 @@
+<?php 
+
+$connection = new PDO(
+	'mysql:host=localhost; dbname=employees',
+	'vagrant',
+	'vagrant'
+);
+
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// connection successful message
+echo $connection->getAttribute(PDO::ATTR_CONNECTION_STATUS), PHP_EOL;
+} catch (PDOException $e) {
+	echo $e->getMessage(), PHP_EOL;
+}
+
+
+ ?>
